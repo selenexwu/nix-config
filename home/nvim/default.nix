1cbea06d13d60dcd068b471f32cb4db74463ef62
Seb <sebastianxwu@gmail.com>
2023-09-01 02:42:25 -0400

    astronvim without user config

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -6,7 +6,22 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    extraPackages = [ pkgs.xclip ];
+    extraPackages = [ pkgs.xclip pkgs.gnumake pkgs.gcc pkgs.cargo ];
   };
+
+  xdg.configFile.nvim = {
+    source = let astronvim = pkgs.fetchFromGitHub {
+        owner = "AstroNvim";
+        repo = "AstroNvim";
+        rev = "v3.36.6";
+        sha256 = "sha256-XbvqX7xEdgfS8/fNvkwB4x7SW4S/Myh3MJS8TH70Xs0=";
+    };
+    in astronvim;
+    # recursive = true;
+  };
+  # xdg.configFile."nvim/lua/user" = {
+  #   source = ./user;
+  #   # recursive = true;
+  # };
 
 }
