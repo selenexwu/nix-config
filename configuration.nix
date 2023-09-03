9c06487a7fc0004bfd45051b38a919efba24fef9
Seb <sebastianxwu@gmail.com>
2023-09-02 23:36:04 -0400

    steam

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,12 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
