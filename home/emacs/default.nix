a1cfa36498f11638ce801c21981b8a4674c2138a
Seb <sebastianxwu@gmail.com>
2025-01-21 22:59:23 -0500

    better doom emacs configs

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -30,48 +30,8 @@
   xdg = {
     enable = true;
     configFile = {
-      "doom-config/config.el".source = ./doom.d/config.el;
-      "doom-config/init.el" = {
-        source = ./doom.d/init.el;
-        onChange = "${pkgs.writeShellScript "doom-config-init-change" ''
-          export PATH="${config.services.emacs.package}/bin:$PATH"
-          export PATH="${config.programs.git.package}/bin:$PATH"
-          export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
-          export DOOMLOCALDIR="${config.home.sessionVariables.DOOMLOCALDIR}"
-          export DOOMPROFILELOADFILE="${config.home.sessionVariables.DOOMPROFILELOADFILE}"
-          ${config.xdg.configHome}/emacs/bin/doom --force sync
-          ${config.xdg.configHome}/emacs/bin/doom env
-        ''}";
-      };
-      "doom-config/packages.el" = {
-        source = ./doom.d/packages.el;
-        onChange = "${pkgs.writeShellScript "doom-config-packages-change" ''
-          export PATH="${config.services.emacs.package}/bin:$PATH"
-          export PATH="${config.programs.git.package}/bin:$PATH"
-          export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
-          export DOOMLOCALDIR="${config.home.sessionVariables.DOOMLOCALDIR}"
-          export DOOMPROFILELOADFILE="${config.home.sessionVariables.DOOMPROFILELOADFILE}"
-          ${config.xdg.configHome}/emacs/bin/doom --force sync -u
-          ${config.xdg.configHome}/emacs/bin/doom env
-        ''}";
-      };
-      "emacs" = {
-        source = inputs.doomemacs;
-        onChange = "${pkgs.writeShellScript "doom-change" ''
-          export PATH="${config.services.emacs.package}/bin:$PATH"
-          export PATH="${config.programs.git.package}/bin:$PATH"
-          export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
-          export DOOMLOCALDIR="${config.home.sessionVariables.DOOMLOCALDIR}"
-          export DOOMPROFILELOADFILE="${config.home.sessionVariables.DOOMPROFILELOADFILE}"
-          if [ ! -d "$DOOMLOCALDIR" ]; then
-            ${config.xdg.configHome}/emacs/bin/doom --force install
-          else
-            ${config.xdg.configHome}/emacs/bin/doom --force clean
-            ${config.xdg.configHome}/emacs/bin/doom --force sync -u
-          fi
-          ${config.xdg.configHome}/emacs/bin/doom env
-        ''}";
-      };
+      "doom-config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/nix-config/home/emacs/doom.d";
+      "emacs".source = inputs.doomemacs;
     };
   };
 
