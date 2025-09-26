29e8f59c23843f16e55f9f7e0f31c54da9fef1b2
Seb <sebastianxwu@gmail.com>
2025-09-26 10:28:07 -0400

    zsh dotDir change

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -8,7 +8,7 @@
   programs.zsh = {
     enable = true;
     autocd = true;
-    dotDir = ".config/zsh";
+    dotDir = "${config.xdg.configHome}/zsh";
     autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
