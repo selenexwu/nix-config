e384ec3304a814036775ee03f443d9673cd091ba
Selene <sebastianxwu@gmail.com>
2026-05-25 15:21:48 -1000

    switch to iwd backend (wpa_supplicant being weird before)

--- configuration.nix
+++ configuration.nix
@@ -170,7 +170,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
 
   # Set your time zone.
   # time.timeZone = lib.mkDefault "America/New_York";
